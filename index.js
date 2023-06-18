693665d40077aa5901d8e1a2f8f1f2ce16b6c927
vaibhav dixit <dixitvaibhav913@gmail.com>
2023-06-18 14:49:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    var time = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("clock").innerHTML = time;
+}
+
+setInterval(updateClock, 1000);
